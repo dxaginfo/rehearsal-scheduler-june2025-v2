448590e36b06005fb718882c17df8e418a93be9d
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-20 17:36:49 -0300

    Add User model with authentication capabilities

---
+++ server/src/models/user.js
@@ -0,0 +1,115 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [8, 100]
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    role: {
+      type: DataTypes.ENUM('user', 'admin'),
+      defaultValue: 'user'
+    },
+    profileImage: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    timezone: {
+      type: DataTypes.STRING,
+      defaultValue: 'UTC'
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    lastLogin: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
+  });
+
+  User.prototype.validatePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+  User.prototype.toJSON = function() {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  };
+
+  User.associate = function(models) {
+    // User belongs to many Bands through UserBand join table
+    User.belongsToMany(models.Band, {
+      through: 'UserBand',
+      as: 'bands',
+      foreignKey: 'userId'
+    });
+    
+    // User has many Availabilities
+    User.hasMany(models.Availability, {
+      foreignKey: 'userId'
+    });
+    
+    // User has many Rehearsals through UserRehearsal join table
+    User.belongsToMany(models.Rehearsal, {
+      through: 'UserRehearsal',
+      as: 'rehearsals',
+      foreignKey: 'userId'
+    });
+  };
+
+  return User;
+};
